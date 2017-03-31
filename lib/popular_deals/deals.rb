b5e8974da7a8aba076ef1a837bac781536fc2cd3
Hima Chitalia <hima_chhag@yahoo.com>
2017-03-31 18:56:58 +0000

    Improving Scarpper

--- lib/popular_deals/deals.rb
+++ lib/popular_deals/deals.rb
@@ -14,12 +14,24 @@ class PopularDeals::Deals
 
   def self.scrap_slickdeals
     doc = Nokogiri::HTML(open("https://slickdeals.net/deals/"))
+    deal = doc.css("div.dealRow").first
     binding.pry
   end
 end
 
+#deal = doc.css("div.dealRow").first
+#title = deal.css("div.dealTitle a.track-popularDealLink").text - works
+#url= deal.css("div.dealTitle a").attribute("href").value -works
+#deal_rating = deal.css("div.ratingCol div.num").text -works
+#price = deal.css("div.priceCol div.price").text -works
+#posted = deal.css("div.dealLinks").first.text.strip
+#new_string = posted.split
+#date = "#{new_string[0]} #{new_string[1]}"
 
-#title = doc.css("div.dealTitle a.track-popularDealLink").first.text - works
+#deal = doc.css("div.dealRow").first
+#title = deal.css("div.dealTitle a.track-popularDealLink").text - works
 #url= doc.css("div.dealTitle a").first.attribute("href").value -works
 #deal_rating = doc.css("div.ratingCol div.num").first.text -works
 #price = doc.css("div.priceCol div.price").first.text -works
+#posted = doc.css("div.priceCol div.price").first.text
+#date = posted..sub /More info.*/, ''
