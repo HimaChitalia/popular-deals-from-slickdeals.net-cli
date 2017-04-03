49cf8d2ee17d25784c86c2919d0d1b4dd898b89c
Hima Chitalia <hima_chhag@yahoo.com>
2017-04-03 15:55:42 +0000

    Cleaned up cli

--- lib/popular_deals/cli.rb
+++ lib/popular_deals/cli.rb
@@ -7,26 +7,15 @@ class PopularDeals::CLI
     list_deals
     menu
     goodbye
-    #PopularDeals::NewDeals.deals
   end
 
   def list_deals
     puts "Today's popular deals are:"
-    #@deals = PopularDeals::NewDeals.new_deals
-    #@deals.each.with_index(1) do |deal, i|
-      #binding.pry
       @deals = PopularDeals::NewDeals.new_deals
       @deals.each do |info|
         info.each.with_index(1) do |deal, i|
-    #     puts "I am checked!"
-         puts "#{i}. #{deal.title} - rating: #{deal.deal_rating}."
-    #     #puts "#{i}. #{r.title} for #{r.price} with a rating of #{r.deal_rating}"
-    #   #puts "#{deal.title} - #{deal.price} - Rating: #{deal.deal_rating}"
-    #   # puts "Available for #{deal.price}"
-    #   # puts "Rating: #{deal.deal_rating}"
-    #   # puts "#{deal.title}"
-    #   # puts "Available at "
-      end
+          puts "#{i}. #{deal.title} - rating: #{deal.deal_rating}."
+        end
      end
   end
 
@@ -45,17 +34,6 @@ class PopularDeals::CLI
     end
   end
 
-  # def open_deal_page(input)
-  #   index = input.to_i - 1
-  #   @deals = PopularDeals::NewDeals.new_deals
-  #      @deals.each do |info|
-  #          d = info[index]
-  #         @product_url = "#{d.url}"
-  #      end
-  #      @product_url.to_s
-  #      puts "They got me!"
-  # end
-
   def disply_deal(input, product_url)
     puts "They got me too!"
     @deal = PopularDeals::NewDeals.deal_page(input, product_url)
@@ -71,49 +49,7 @@ class PopularDeals::CLI
     puts "-------------------------------------"
     puts ""
 
-    #puts "#{@deal}"
-    # @deal.each do |key, info|
-    #   puts "#{info}"
-      # puts "Deal: #{info[1]}"
-      # puts "Deal details: #{info[3]}"
-      #puts "#{info}"
-      # info.each do |d|
-      #   puts "#{d[0]}#{d[1]}#{d[2]}"
-        #d
-  #     puts "I am checked!"
-       #puts "#{d.name}."
-  #     #puts "#{i}. #{r.title} for #{r.price} with a rating of #{r.deal_rating}"
-  #   #puts "#{deal.title} - #{deal.price} - Rating: #{deal.deal_rating}"
-  #   # puts "Available for #{deal.price}"
-  #   # puts "Rating: #{deal.deal_rating}"
-  #   # puts "#{deal.title}"
-  #   # puts "Available at "
-    #end
-  #  end #{info.discription}"
-      # puts "To get this deal, visit: #{info.purchase}"
-    end
-    #   deal.each do |info|
-    #  puts "I am checked!"
-    #     puts "#{info}."
-     #puts "#{i}. #{deal.title} - rating: #{deal.deal_rating}."
-#     #puts "#{i}. #{r.title} for #{r.price} with a rating of #{r.deal_rating}"
-#   #puts "#{deal.title} - #{deal.price} - Rating: #{deal.deal_rating}"
-#   # puts "Available for #{deal.price}"
-#   # puts "Rating: #{deal.deal_rating}"
-#   # puts "#{deal.title}"
-#   # puts "Available at "
-  #end
-
-    #end
-  #end
-
-  # def disply_deal(input)
-  #   @deals = PopularDeals::NewDeals.new_deals
-  #   @deals.each do |info|
-  #       d = info[input.to_i - 1]
-  #      puts "#{d.title}."
-  #   end
-  # end
+  end
 
   def goodbye
     puts "Come back again for more deals. Have a great day!"
