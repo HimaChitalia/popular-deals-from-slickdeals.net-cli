b23fc062a97cb4db2f7dcfb1bfe2e7a76c219f58
Hima Chitalia <hima_chhag@yahoo.com>
2017-04-07 00:46:42 +0000

    Added 3 more pages

--- lib/popular_deals/cli.rb
+++ lib/popular_deals/cli.rb
@@ -2,6 +2,9 @@
 class PopularDeals::CLI
   BASE_URL = "https://slickdeals.net/deals/"
   PAGE2_URL = "https://slickdeals.net/deals/?page=2&sort=recent"
+  PAGE3_URL = "https://slickdeals.net/deals/?page=3&sort=recent"
+  PAGE4_URL = "https://slickdeals.net/deals/?page=4&sort=recent"
+  PAGE5_URL = "https://slickdeals.net/deals/?page=5&sort=recent"
   attr_accessor :product_url
   #BROWSER=(/usr/local/bin/firefox-bin -new-tab '%s':/usr/local/bin/google-chrome-stable)
 
@@ -9,24 +12,48 @@ class PopularDeals::CLI
     select_list_of_deals
     list_deals
     menu
+    #goodbye
   end
 
   def select_list_of_deals
     puts ""
-    puts "What number restaurants would you like to see? 1-20 or 21-40?".upcase.yellow
+    puts "Which list of the deals would you like to see?".upcase.cyan.bold
+    puts "Type 1 for the first list of 1-20 deals.".yellow
+    puts "Type 2 for the second list of 21-40 deals.".yellow
+    puts "Type 3 for the third list of 41-60 deals.".yellow
+    puts "Type 4 for the fourth list of 61-80 deals.".yellow
+    puts "Type 5 for the fifth list of 81-100 deals.".yellow
     puts ""
     input = gets.strip.to_i
     case input
-    when 1 || 1-20 || 1 - 20
+    when 1
       @deals = @deals = PopularDeals::NewDeals.scrap_slickdeals(BASE_URL)
       puts ""
       puts "---------- Deal list #{input} - #{input+19} ----------"
       puts ""
-    when 21 || 21-40 || 21 -40
+    when 2
       @deals = @deals = PopularDeals::NewDeals.scrap_slickdeals(PAGE2_URL)
       puts ""
       puts "---------- Deal list #{input} - #{input+19} ----------"
       puts ""
+    when 3
+      @deals = @deals = PopularDeals::NewDeals.scrap_slickdeals(PAGE3_URL)
+      puts ""
+      puts "---------- Deal list #{input} - #{input+19} ----------"
+      puts ""
+    when 4
+      @deals = @deals = PopularDeals::NewDeals.scrap_slickdeals(PAGE4_URL)
+      puts ""
+      puts "---------- Deal list #{input} - #{input+19} ----------"
+      puts ""
+    when 5
+      @deals = @deals = PopularDeals::NewDeals.scrap_slickdeals(PAGE5_URL)
+      puts ""
+      puts "---------- Deal list #{input} - #{input+19} ----------"
+      puts ""
+    else
+      puts "Don't understand that command"
+      select_list_of_deals
     end
     @deals
   end
@@ -74,29 +101,52 @@ class PopularDeals::CLI
     while input != "exit"
 
       puts ""
-      puts "Enter the number of deal you would like more info on or type Exit".light_blue.bold
+      puts "Enter the number of deal you would like more info on or type Exit.".light_blue.bold
       puts ""
         input = gets.strip.downcase
         puts ""
+        #unless input == "exit"
+          if input.to_i > 0 && input.to_i <= 20
+            puts ""
+            puts "-----------------------------------------------------------------------------------------------------------"
+            puts ""
+            puts "Please see below details of deal no. #{input}".upcase.cyan.bold
+              disply_deal(BASE_URL, input, product_url)
+              #open_deal_in_browser
+          elsif input == "list"
+            list_deals
+          elsif input == "select list"
+            call
+          elsif input == "exit"
+            goodbye
 
-        if input.to_i > 0 && input.to_i <= 20
-          puts ""
-          puts "-----------------------------------------------------------------------------------------------------------"
-          puts ""
-          puts "Please see below details of deal no. #{input}".upcase.cyan.bold
-            disply_deal(BASE_URL, input, product_url)
-            #open_deal_in_browser
-        elsif input == "list".downcase
-          list_deals
-        elsif input == "select list".downcase 
-          call
-        elsif input == "exit"
-          goodbye
-        else
-          puts "Don't understand your command. Type select list to see available deal lists, type list to see the current deal list or exit".colorize(:color => :white, :background => :red)
-          puts ""
-        end
-    end
+          else
+            puts "Don't understand your command.".colorize(:color => :white, :background => :red)
+            puts "Type 'SELECT LIST' to see available deal lists.".yellow
+            puts "Type 'LIST' to see the current deal list.".yellow
+            puts "Type 'EXIT' to exit.".yellow
+            puts ""
+          end
+      end
+        # if input.to_i > 0 && input.to_i <= 20
+        #   puts ""
+        #   puts "-----------------------------------------------------------------------------------------------------------"
+        #   puts ""
+        #   puts "Please see below details of deal no. #{input}".upcase.cyan.bold
+        #     disply_deal(BASE_URL, input, product_url)
+        #     #open_deal_in_browser
+        # elsif input == "list"
+        #   list_deals
+        # elsif input == "select list"
+        #   call
+        # elsif input == "exit"
+        #   goodbye
+        #   break
+        # else
+        #   puts "Don't understand your command. Type select list to see available deal lists, type list to see the current deal list or exit".colorize(:color => :white, :background => :red)
+        #   puts ""
+        # end
+    #end
   end
 
   # def open_deal_in_browser
