b6a6e3935914f1d30f85d5ee5b047d8fddeb6c1e
Hima Chitalia <hima_chhag@yahoo.com>
2017-04-08 02:15:27 +0000

    Deleted commented text

--- lib/popular_deals/newdeals.rb
+++ lib/popular_deals/newdeals.rb
@@ -12,47 +12,11 @@ class PopularDeals::NewDeals
     @@all
   end
 
-    # def self.scrap_slickdeals(base_url)
-  #   doc = Nokogiri::HTML(open(base_url))
-  #     deals = []
-  #     all_deals = doc.css("div.dealRow")
-  #     all_deals.collect do |one_deal|
-  #     deal = self.new
-  #     deal.title = one_deal.css("div.dealTitle a.track-popularDealLink").text.strip
-  #     link = one_deal.css("div.dealTitle a").attribute("href").value
-  #     deal.url = "https://slickdeals.net#{link}"
-  #     deal.deal_rating = one_deal.css("div.ratingCol div.num").text.strip
-  #     deal.price = one_deal.css("div.priceCol div.price").text.strip
-  #
-  #     date = one_deal.css("div.dealLinks").first.text.strip
-  #     new_array = date.split
-  #     deal.posted = "#{new_array[0]} #{new_array[1]}"
-  #     deals << deal
-  #   end
-  #   deals
-  # end
-
   def self.open_deal_page(base_url, input)
     index = input.to_i - 1
     deals = self.all
-        product_url = "#{deals[index].url}"
-        product_url
+    product_url = "#{deals[index].url}"
+    product_url
   end
-
-  # def self.deal_page(base_url, input, product_url)
-  #   self.open_deal_page(base_url, input)
-  #   deal = {}
-  #   html = open(@product_url)
-  #   doc = Nokogiri::HTML(html)
-  #   data = doc.text.strip
-  #   deal[:name] = doc.css("h1").text.strip
-  #   deal[:discription] = doc.css(".textDescription").text.strip
-  #     if doc.at_css("a#largeBuyNow").nil?
-  #     deal[:purchase] = @product_url
-  #   else
-  #     deal[:purchase] = doc.at_css("a#largeBuyNow").attr("href")
-  #   end
-  #  deal
-  # end
 
 end
